ed14f073c94c9e49b8d3f929abb87eb037b9784e
Alessio Langellotti <alessiolangellotti2005@gmail.com>
2022-05-07 08:46:31 +0200

    update diario.md

--- diario.md
+++ diario.md
@@ -8,5 +8,17 @@
 - Creazione e conclusione classe Cassa (Plaka e Langellotti)
 - Creazione classe Supermercato (Rampazzo e Langellotti)
 - creazione Toolbar GUI (Salvi)
-
-
+## 2 aprile
+- Associazione all'evento scatenato dalla pressione di un Button all' apertura e chiusura della cassa (Langellotti)
+- timelineAvanzamento e timelineAggiuntaCarrello (Plaka)
+- Supermercato::aggiungiCarrello e SupermercatoController::aggiungiCarrello (Rampazzo e Salvi)
+- start e pause menuItem (Plaka)
+- Supermercato::getMax e Supermercato::getMinAperta (Rampazzo, Salvi e Langellotti)
+## 9 aprile
+- Aggiunta clienti arrabbiati (Langellotti)
+- synchronized supermercato (Plaka)
+- distribuzione in funzioni codice ripetuto nel controller (Plaka)
+- Supermercato::bilanciaCarrelli (Rampazzo, Salvi e Langellotti)
+## 16 aprile
+- reset simulazione (Rampazzo, Salvi)
+- aggiungi e elimina cassa (Langellotti e Plaka)
--- src/main/java/net/supermercato/SupermercatoController.java
+++ src/main/java/net/supermercato/SupermercatoController.java
@@ -12,26 +12,26 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
-import java.util.ArrayList;
 
 public class SupermercatoController {
-    private Supermercato supermercato;
     private final int NCasseIniziali = 10;
-    public static final int QTOLTA = 1;
+    private Supermercato supermercato;
+    public static final int QTOLTA = 5;
     public static final int MAXELEMENTINELCARRELLO = 50;
-    public static final double tempoArrivoCarrello = 0.5;
-    public static final double tempoAvanzamento = 0.1;
+    public static double tempoArrivoCarrello = 1;
+    public static double tempoAvanzamento = 1;
 
     private Timeline timelineAggiuntaCarrello, timelineAvanzamento;
     @FXML
-    Label lblArrabbiati;
-    @FXML
-    MenuItem startPause;
-    @FXML
     HBox HboxCasse;
-    ArrayList<Button> buttonsCasse;
     @FXML
-    MenuItem aggiungiCassa;
+    MenuItem startPause, aggiungiCassa;
+    @FXML
+    Label lblArrabbiati;
+
+    Button getButtonCassa(int indice) {
+        return (Button) HboxCasse.getChildren().get(indice);
+    }
 
     public Button creaButtonCassa(int id) {
         Button button = new Button("0");
@@ -44,8 +44,7 @@ public class SupermercatoController {
                 if (supermercato.getCassa(index_cassa).isAperta()) {
                     supermercato.chiudiCassa(index_cassa);
                     premuto.setBackground(new Background(new BackgroundFill(Color.rgb(255, 0, 0, 0.7), new CornerRadii(5.0), new Insets(-5.0))));
-                }
-                else {
+                } else {
                     supermercato.apriCassa(index_cassa);
                     premuto.setBackground(new Background(new BackgroundFill(Color.rgb(0, 255, 0, 0.7), new CornerRadii(5.0), new Insets(-5.0))));
                     if (supermercato.getNArrabbiati() > 0)
@@ -57,24 +56,20 @@ public class SupermercatoController {
         return button;
     }
 
-    private void creaSupermercato(int MAXCASSE){
+    private void creaSupermercato(int MAXCASSE) {
         supermercato = new Supermercato(MAXCASSE);
-        buttonsCasse = new ArrayList<>();
-        for (int i = 0; i < supermercato.getMAXCASSE(); i++)
-            buttonsCasse.add(creaButtonCassa(i));
-
         HboxCasse.getChildren().clear();
-        HboxCasse.getChildren().addAll(buttonsCasse);
+        for (int i = 0; i < supermercato.getMAXCASSE(); i++)
+            HboxCasse.getChildren().add(creaButtonCassa(i));
+
         HboxCasse.setSpacing(20);
         HboxCasse.setAlignment(Pos.CENTER);
         supermercato.apriCassa(0);
-        buttonsCasse.get(0).setBackground(new Background(new BackgroundFill(Color.rgb(0, 255, 0, 0.7), new CornerRadii(5.0), new Insets(-5.0))));
+        getButtonCassa(0).setBackground(new Background(new BackgroundFill(Color.rgb(0, 255, 0, 0.7), new CornerRadii(5.0), new Insets(-5.0))));
     }
 
-    @FXML
-    void initialize() {
-        creaSupermercato(NCasseIniziali);
-        timelineAggiuntaCarrello = new Timeline(new KeyFrame(
+    private KeyFrame keyFrameTimelineAggiuntaCarrello() {
+        return new KeyFrame(
                 Duration.seconds(tempoArrivoCarrello),
                 e -> {
                     synchronized (supermercato) {
@@ -82,8 +77,11 @@ public class SupermercatoController {
                         aggiornavaloricasse();
                     }
                 }
-        ));
-        timelineAvanzamento = new Timeline(new KeyFrame(
+        );
+    }
+
+    private KeyFrame keyFrameTimelineAvanzamento() {
+        return new KeyFrame(
                 Duration.seconds(tempoAvanzamento),
                 e -> {
                     synchronized (supermercato) {
@@ -91,7 +89,14 @@ public class SupermercatoController {
                         aggiornavaloricasse();
                     }
                 }
-        ));
+        );
+    }
+
+    @FXML
+    void initialize() {
+        creaSupermercato(NCasseIniziali);
+        timelineAggiuntaCarrello = new Timeline(keyFrameTimelineAggiuntaCarrello());
+        timelineAvanzamento = new Timeline(keyFrameTimelineAvanzamento());
 
         timelineAggiuntaCarrello.setCycleCount(Animation.INDEFINITE);
         timelineAvanzamento.setCycleCount(Animation.INDEFINITE);
@@ -99,27 +104,25 @@ public class SupermercatoController {
 
     private void aggiornavaloricasse() {
         for (int i = 0; i < supermercato.getMAXCASSE(); i++)
-            buttonsCasse.get(i).setText("" + supermercato.getCassa(i).getNCarrelli());
+            getButtonCassa(i).setText("" + supermercato.getCassa(i).getNCarrelli());
         lblArrabbiati.setText("Clienti arrabbiati in attesa dell'apertura di una cassa: " + supermercato.getNArrabbiati());
     }
 
     @FXML
-    private void onAggiungiCassa(){
-        buttonsCasse.add(creaButtonCassa(buttonsCasse.size()));
+    private void onAggiungiCassa() {
         supermercato.aggiungiCassa();
-        HboxCasse.getChildren().add(buttonsCasse.get(supermercato.getMAXCASSE()));
-        supermercato.setMAXCASSE(supermercato.getMAXCASSE()+1);
+        HboxCasse.getChildren().add(creaButtonCassa(supermercato.getMAXCASSE()));
+        supermercato.setMAXCASSE(supermercato.getMAXCASSE() + 1);
     }
 
     public void onEliminaCassa() {
-        if (buttonsCasse.size()<1) return;
+        if (HboxCasse.getChildren().size() < 1) return;
         supermercato.rimuoviUltimaCassa();
-        supermercato.setMAXCASSE(supermercato.getMAXCASSE()-1);
+        supermercato.setMAXCASSE(supermercato.getMAXCASSE() - 1);
         if (supermercato.getNArrabbiati() > 0)
             supermercato.assegnaArrabbiati();
         supermercato.bilanciaCarrelli();
-        buttonsCasse.remove(buttonsCasse.size()-1);
-        HboxCasse.getChildren().remove(HboxCasse.getChildren().size()-1);
+        HboxCasse.getChildren().remove(HboxCasse.getChildren().size() - 1);
         aggiornavaloricasse();
     }
 
